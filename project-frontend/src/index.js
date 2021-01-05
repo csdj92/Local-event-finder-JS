a0aa19caa3c3d9e1c9411f976c283185ceb41693
csdj92 <csdj92@gmail.com>
2021-01-05 12:32:27 -0500

    inital commit

--- project-frontend/src/index.js
+++ project-frontend/src/index.js
@@ -1,43 +1,75 @@
 const BASE_URL = 'http:localhost:3000'
 const main = document.getElementById('main')
+const form = document.querySelector('form');
 
-function getEvents(){
+function getEvents() {
     main
-    fetch(BASE_URL + '/events')
-    .then(res => res.json())
-    .then(events => { events.map(events => {
-        main.innerHTML += `
+    fetch(BASE_URL + '/cities')
+        .then(res => res.json())
+        .then(events => {
+            events.map(events => {
+                main.innerHTML += `
        <li>
-       <a href="#" data-id=${events.id}>${events.description}</a>
+       <a href="#" data-id=${events.id}>${events.name}</a>
        </li>
        `
-    }); eventLink()
-})
+            });
+            eventLink()
+        })
 }
 getEvents()
 
-function eventLink(){
+function eventLink() {
     const events = document.querySelectorAll("li a")
     events.forEach(events => {
         events.addEventListener('click', displayEvents)
     })
 }
 
-function displayEvents(e){
+function displayEvents(e) {
     console.log(e.target);
-let id = e.target.dataset.id
-main
-main.innerHTML = ""
-fetch(BASE_URL + `/events/${id}`)
-.then(resp => resp.json())
-.then(event => {
-    console.log(event.time);
-    main.innerHTML = `
-    <h1>${event.name}</h1>
-    <h2>${event.description}</h2>
-    <h2>${event.time}</h2>
-
-
-    `
-})
+    let id = e.target.dataset.id
+    main
+    main.innerHTML = ""
+    fetch(BASE_URL + `/cities/${id}`)
+        .then(resp => resp.json())
+        .then(city => { 
+           
+            city.events.forEach(event => {
+                main.innerHTML += `
+                <li>${event.name}</li>
+                <li>${event.description}</li>
+                `
+            })        })
 }
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const name = formData.get('name');
+    const description = formData.get('description');
+    const date = formData.get('date');
+    const time = formData.get('time');
+    //const city = formData.get('city')
+
+    const makeEvent = {
+        name,
+        description,
+        date,
+        time,
+        
+    };
+
+    fetch(BASE_URL + '/events', {
+            method: "POST",
+            body: JSON.stringify(makeEvent),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(resp => resp.json())
+        .then(event => {
+            console.log(event);
+
+        }).catch(Error)
+})
