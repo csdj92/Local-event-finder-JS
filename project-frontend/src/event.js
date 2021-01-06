17398049078d55f73952ab7e94d2ff1d575ecf45
csdj92 <csdj92@gmail.com>
2021-01-06 11:24:21 -0500

    POST new event

--- project-frontend/src/event.js
+++ project-frontend/src/event.js
@@ -1,3 +1,5 @@
+const EVENT_URL = 'http:localhost:3000/events'
+
 class Event { 
     constructor(data){
         this.id = data.id
@@ -8,4 +10,47 @@ class Event {
         this.time = data.time
     }
 }
+
+function addEvent() {
+    const event = {
+        name: document.getElementById('name').value,
+        description: document.getElementById('description').value,
+        date: document.getElementById('date').value,
+        time: document.getElementById('time').value,
+        city_id: document.getElementById('event-cityId').value       
+
+    }
+    fetch(EVENT_URL,{
+        method: 'POST',
+        body: JSON.stringify(event),
+        headers:{'Content-Type': 'application/json'}
+    })
+    .then(resp => resp.json())
+    .then(event => {
+        console.log(event)
+        clearPage()
+        getCities()
+    });
+}
+
+function renderEventForm(cityId) {
+    return `
+    <label><strong>Name: </strong></label><br/>
+    <input type="text" id="name"><br/>
+
+    <input type="hidden" id="event-cityId" value="${cityId}">
+
+    <label><strong>Description:   </strong></label><br/>
+    <input type="text" id="event-description"><br/>
+
+    <label><strong>Date:   </strong></label><br/>
+    <input type="text" id="event-date"><br/>
+
+    <label><strong>Time:   </strong></label><br/>
+    <input type="text" id="event-time"><br/>   
+
+    <input type="submit" value="Submit" style="color:white;background-color:orange">
+    `  
+}
+
 
