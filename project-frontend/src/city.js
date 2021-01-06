87128a8a96c9154f00da47e43f8cd9690a097e0d
csdj92 <csdj92@gmail.com>
2021-01-06 09:38:25 -0500

    get and post a city

--- project-frontend/src/city.js
+++ project-frontend/src/city.js
@@ -8,3 +8,59 @@ const cityForm = `
 <input type "text" id="cityName"><br>
 <input type="hidden" id="cityId"></input>
 `
+
+class City{
+    constructor(data){
+        this.name = data.name
+    }
+
+    static newCityForm() {
+    let newCityForm = document.getElementById('new-form')
+    newCityForm.innerHTML = `
+    <form onsubmit="createCity(); return false;">` + 
+    cityForm + 
+    `<input type="submit" value="Add New City" >
+    </form>
+    <br/>`
+}
+    static editCityForm(){
+        let editCityForm = document.getElementById("new-form")
+        editCityForm.innerHTML = `
+        <form onsubmit="createCity(); return false;">` +
+        cityForm +
+        `<input type="submit" value="Update City"        
+        </form>`
+    }
+
+
+}
+
+
+function getCities() {    
+    fetch(BASE_URL + '/cities')
+    .then(res => res.json())
+    .then(cities => {
+        renderCities(cities)
+        //eventlistner??
+    })      
+}
+
+function createCity() {
+    const city = {
+        name: document.getElementById('cityName').value,
+      // add more to a city?
+
+    }
+
+    fetch(BASE_URL + '/cities', {
+        method: 'POST',
+        body: JSON.stringify(city),
+        headers:{'Content-Type': 'application/json'}
+    })
+    .then(res => res.json())
+    .then(city => {
+        clearpage()
+        getCities()
+        City.newCityForm()
+    });
+}
