e689b2f30591e01f5d5018b8c23241406a0bd029
csdj92 <csdj92@gmail.com>
2021-01-06 09:07:06 -0500

    separation of responsibilities

---
+++ project-frontend/src/city.js
@@ -0,0 +1,10 @@
+const BASE_URL = 'http:localhost:3000'
+const main = document.getElementById('main')
+const form = document.querySelector('form');
+const container = document.querySelector('container')
+
+const cityForm = `
+<label>City Name: </label><br>
+<input type "text" id="cityName"><br>
+<input type="hidden" id="cityId"></input>
+`
