b31ec689a6e6f8dcd0f2886c716c47a37160ac6b
csdj92 <csdj92@gmail.com>
2021-01-06 11:11:29 -0500

    inital commit for events

---
+++ project-frontend/src/event.js
@@ -0,0 +1,11 @@
+class Event { 
+    constructor(data){
+        this.id = data.id
+        this.name = data.name
+        this.city_id = data.city_id
+        this.description = data.description
+        this.date = data.date
+        this.time = data.time
+    }
+}
+
